547166d2569c13f192a47da3f0580abab171c87b
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-02-25 17:05:48 +0100

    #5 kreiran webapi u .NET

---
+++ sandbox/bogdan_andjelkovic/back/API/Controller/DevicesController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controller;
+
+[Controller]
+[Route("[controller]")]
+public class DevicesController : ControllerBase
+{
+    private DataContext _connection;
+    
+    public DevicesController(DataContext connection)
+    {
+        this._connection = connection;
+    }
+    
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Device>>> getDevices()
+    {
+        return await this._connection.Devices.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Device>> getDevice(int id)
+    {
+        return await this._connection.Devices.FindAsync(id);
+    }
+}
