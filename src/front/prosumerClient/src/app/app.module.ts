a48d6df7041f51eaaa9a954c3ac979954ac2d0a9
Nemanja Zivkovic <84846578+Nexi7@users.noreply.github.com>
2023-03-08 20:33:50 +0100

    #23 zapocet rad na izgledu login forme

--- src/front/prosumerClient/src/app/app.module.ts
+++ src/front/prosumerClient/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule, Routes } from '@angular/router';  
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
