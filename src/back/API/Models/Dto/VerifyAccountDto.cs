4f3d93c1260a547b1cafce616de9752c9a1e9dc7
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-03-15 23:49:30 +0100

    dev: changed verification, now request has token instead of email

--- src/back/API/Models/Dto/VerifyAccountDto.cs
+++ src/back/API/Models/Dto/VerifyAccountDto.cs
@@ -2,7 +2,7 @@
 
 public class VerifyAccountDto
 {
-    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
     public string Password1 { get; set; } = string.Empty;
     public string Password2 { get; set; } = string.Empty;
 }
