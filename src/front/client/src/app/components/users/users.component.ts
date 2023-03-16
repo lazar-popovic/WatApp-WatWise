887dd0bfcf5d34c95c8c829e6ac86b375dfc4ef7
Aleksandar Djalovic <djalovicaleksandar01@gmail.com>
2023-03-16 14:17:40 +0100

    #44 verification and registration logic connected to api.

--- src/front/client/src/app/components/users/users.component.ts
+++ src/front/client/src/app/components/users/users.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  user: any = {
+    email: '',
+    firstname: '',
+    lastname: '',
+  };
 
+  constructor(private userService: UserService,private router: Router) { }
+
+  storeUser()
+  {
+    this.userService.createUser(this.user).subscribe((result: any) => {
+      this.router.navigateByUrl('/dso/users');
+    },(error: any) => {
+      console.log(error);
+    });
+  }
 }
