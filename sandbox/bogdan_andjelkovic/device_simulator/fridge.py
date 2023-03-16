f529ebcfb9878ba41945a8c3b1ed5ac978842cce
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-03-16 12:11:08 +0100

    #39 Simulated work of Fridge via python script( created JSON file)

--- sandbox/bogdan_andjelkovic/device_simulator/fridge.py
+++ sandbox/bogdan_andjelkovic/device_simulator/fridge.py
@@ -5,11 +5,8 @@ from datetime import datetime, timedelta
 # Define constants for the script
 NUM_DAYS = 365
 NUM_HOURS_PER_DAY = 24
-WATER_HEATER_MIN_CONSUMPTION = 3.0
-WATER_HEATER_MAX_CONSUMPTION = 5.0
-DAYTIME_ACTIVE_HOURS = 5
-DAYTIME_ACTIVE_CONSUMPTION_FACTOR = 1
-OTHER_HOURS_CONSUMPTION_FACTOR = 0.05
+FRIDGE_MIN_CONSUMPTION = 0.5
+FRIDGE_MAX_CONSUMPTION = 1.5
 
 # Define a dictionary to store the consumption data
 consumption_data = {}
@@ -20,7 +17,7 @@ start_date = datetime(2023, 1, 1)
 # Loop over each day in 2022
 for day in range(NUM_DAYS):
     # Define a list to store the consumption data for the day
-    day_consumption = dict()
+    day_consumption = {}
     # Loop over each hour of the day
     for hour in range(NUM_HOURS_PER_DAY):
         # Determine the current time of day (day or night)
@@ -28,21 +25,19 @@ for day in range(NUM_DAYS):
             time_of_day = "day"
         else:
             time_of_day = "night"
-        # Determine the factor to apply to the consumption range based on the time of day and active hours
-        if time_of_day == "day" and hour < 6 + DAYTIME_ACTIVE_HOURS:
-            factor = DAYTIME_ACTIVE_CONSUMPTION_FACTOR
-        else:
-            factor = OTHER_HOURS_CONSUMPTION_FACTOR
+        # Set the factor to 1 for a fridge that works constantly
+        factor = 1
         # Generate a random consumption value between the minimum and maximum, with a factor based on the time of day and active hours
-        consumption = round(random.uniform(WATER_HEATER_MIN_CONSUMPTION * factor, WATER_HEATER_MAX_CONSUMPTION * factor), 2)
-        # Append the consumption value to the day's list
+        consumption = round(random.uniform(FRIDGE_MIN_CONSUMPTION * factor, FRIDGE_MAX_CONSUMPTION * factor), 2)
+        # Format the hour value with leading zeroes
         hour_str = str(hour).zfill(2)
-        day_consumption[hour_str+":00"]=consumption
+        # Append the consumption value to the day's list
+        day_consumption[hour_str+":00"] = consumption
     # Add the day's consumption data to the dictionary with key as date string in mm/dd/yyyy format
     consumption_data[start_date.strftime('%m/%d/%Y')] = day_consumption
     # Increment the start_date to next day
     start_date += timedelta(days=1)
 
 # Write the consumption data to a JSON file
-with open('water_heater_consumption.json', 'w') as outfile:
+with open('fridge_consumption.json', 'w') as outfile:
     json.dump(consumption_data, outfile)
