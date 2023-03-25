fc00d3bad3f3fcbde9d24c820b0e5f9db3670aaf
Aleksandar Djalovic <djalovicaleksandar01@gmail.com>
2023-03-25 17:47:46 +0100

    #70 User get by id added.

--- src/front/client/src/app/services/user.service.ts
+++ src/front/client/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { environment } from '../environments/environment';
+import { User } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class UserService {
   createUser(data: any) : Observable<any>
   {
     return this.http.post(`${environment.apiUrl}auth/register-user`,data, {observe: 'response'});
+  }
+
+  public getUser(id: string | null) : Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}user/${id}`);
   }
 }
