646865548049f7eb9443c081bf10fbf806ff87da
Aleksandar Djalovic <djalovicaleksandar01@gmail.com>
2023-03-27 14:18:28 +0200

    #72 Components operation changed to universal sidebar.

--- src/front/client/src/app/components/overview/overview.component.ts
+++ src/front/client/src/app/components/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-overview',
@@ -7,7 +8,4 @@ import { Component } from '@angular/core';
 })
 export class OverviewComponent 
 {
-    //dsoShow = true;
-    dsoShow = false;
-
 }
