1e51dadc5e485a0231f41f7880b23f461f22e19c
Aleksandar Djalovic <djalovicaleksandar01@gmail.com>
2023-03-28 17:14:40 +0200

    #73 Overview design done.

--- src/front/client/src/app/components/users-overview/users-overview.component.ts
+++ src/front/client/src/app/components/users-overview/users-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/Models/User';
 
 @Component({
   selector: 'app-users-overview',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class UsersOverviewComponent {
 
+  users: User[] = [];
+  currentIndex = 9;
+  pageDist = 2;
+  pagesNum = [1,2,3,4,5,6,7,8,9,10];
+
+  dynamicList: number[] = [];
+
+  constructor() {
+    for(var i = 0; i < 10; i++) {
+      this.users.push(new User());
+    }
+    this.makeList();
+  }
+
+  makeList() {
+    this.dynamicList.push(1,2);
+    if(this.currentIndex < this.pageDist*2+1) {
+      for(var i = 3;i<this.pageDist*2+1;i++) {
+        this.dynamicList.push(i);
+      }
+    }
+    else if(this.currentIndex >= this.pageDist *2) {
+      for(var i = this.currentIndex - this.pageDist;i<this.currentIndex + this.pageDist + 1;i++) {
+        if(i <= this.pagesNum.length - this.pageDist)
+          this.dynamicList.push(i);
+      }
+    }
+    else {
+      for(var i = this.pagesNum.length;i>this.pagesNum.length - this.pageDist*2 - 1;i--) {
+        this.dynamicList.push(i);
+      }
+    }
+    this.dynamicList.push(this.pagesNum.length-1,this.pagesNum.length);
+  }
 }
