4ac4c4efe125a73566073fc7ca97dcfaae5f055b
Lazar Popovic <popovic.lazar98@yahoo.com>
2023-04-17 13:58:59 +0200

    #120 added communication with API via obtained api key rather than hardcoding json string and later parsing it. Added fontawesome for sun,moon and clouds icons

--- sandbox/angular-weather-widget/src/app/components/weather-widget/weather-widget.component.ts
+++ sandbox/angular-weather-widget/src/app/components/weather-widget/weather-widget.component.ts
@@ -17,8 +17,10 @@ export class WeatherWidgetComponent implements OnInit {
 
     getWeatherData()
     {
-      let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
-      this.setWeatherData(data);
+      fetch('https://api.openweathermap.org/data/2.5/weather?q=kragujevac&units=metric&appid=e39e528663e833d20fe7c7aba6813e5e')
+      .then(response=>response.json())
+      .then(data=>{this.setWeatherData(data);})
+
     }
 
     setWeatherData(data: any)
