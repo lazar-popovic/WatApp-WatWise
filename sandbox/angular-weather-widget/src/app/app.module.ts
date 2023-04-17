0702b5d619c5ff2b4c74afbce39e204cc0525fd4
Lazar Popovic <popovic.lazar98@yahoo.com>
2023-04-17 14:55:18 +0200

    #120 Finished with creating 5 day forecast weather widget.Added necessary call to OpenWeatherMap API for data,created html for component and also added some style to make widget responsive.

--- sandbox/angular-weather-widget/src/app/app.module.ts
+++ sandbox/angular-weather-widget/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WeatherWidgetComponent } from './components/weather-widget/weather-widget.component';
+//import { WeatherWidgetComponent } from './components/weather-widget/weather-widget.component';
+import { AppWeatherWidgetComponent } from './components/app-weather-widget/app-weather-widget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherWidgetComponent
+    //WeatherWidgetComponent,
+    AppWeatherWidgetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
