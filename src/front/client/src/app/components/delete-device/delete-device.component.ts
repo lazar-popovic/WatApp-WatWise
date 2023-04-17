b3911434497c9cd6186b6081b172a1ff4ab11dd0
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-04-17 20:56:12 +0200

    dev: Device delete small issue fixed, now displaying delete message and added ngbusy

--- src/front/client/src/app/components/delete-device/delete-device.component.ts
+++ src/front/client/src/app/components/delete-device/delete-device.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { DeviceService } from 'src/app/services/device.service';
+import { ToastrNotifService } from 'src/app/services/toastr-notif.service';
 
 @Component({
   selector: 'app-delete-device',
@@ -11,11 +13,17 @@ export class DeleteDeviceComponent {
   @Output() exitStatusEvent = new EventEmitter<boolean>();
   @Input() id: number = 0;
 
-  constructor(private deviceService: DeviceService, private router: Router) { }
+  busy: Subscription | undefined;
+
+  constructor(private deviceService: DeviceService, private router: Router, private toastrNotifService: ToastrNotifService) { }
 
   deleteDevice() {
-    this.deviceService.deleteDevice(this.id).subscribe((result: any) => {
-      this.router.navigateByUrl('prosumers/devices');
+    this.busy = this.deviceService.deleteDevice(this.id).subscribe((result: any) => {
+      console.log( result);
+      if( result.body.success) {
+        this.toastrNotifService.showSuccess( result.body.data);
+        this.router.navigate(['/prosumer/devices']);
+      }
     }, (error: any) => {
       console.log(error);
   });
