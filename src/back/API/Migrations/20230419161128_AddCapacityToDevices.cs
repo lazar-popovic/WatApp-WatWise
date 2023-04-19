84fae2b313276b8c3d125ae52481c2a176f08119
anica.stojadinovic <anchi_stojadinovic97@yahoo.com>
2023-04-19 18:14:16 +0200

    #111 Add Capacity in Device model and add migration AddCapacityToDevice

---
+++ src/back/API/Migrations/20230419161128_AddCapacityToDevices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCapacityToDevices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Capacity",
+                table: "Devices",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Devices");
+        }
+    }
+}
