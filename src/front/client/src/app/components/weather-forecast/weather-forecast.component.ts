7ff84316bb44138a7c7bf916cb5923206681187a
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-04-19 21:36:59 +0200

    #119 Tried to fix weather issue

--- src/front/client/src/app/components/weather-forecast/weather-forecast.component.ts
+++ src/front/client/src/app/components/weather-forecast/weather-forecast.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherForecastService, Weather, Forecast } from '../../services/weather-forecast.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -9,27 +10,48 @@ import { WeatherForecastService, Weather, Forecast } from '../../services/weathe
 })
 export class WeatherForecastComponent implements OnInit {
 
-  currentWeather: Weather | null = null;
+  currentWeather: Weather | undefined;
   forecasts: Forecast[] = [];
 
+  busyWeather: Subscription | undefined;
+
   constructor(private weatherService: WeatherForecastService) {
-   // this.currentWeather = {};
   }
 
   ngOnInit() {
-    this.weatherService.getWeather()
-      .then((currentWeather: Weather | null) => {
-        if(currentWeather)
-          this.currentWeather = currentWeather;
-        else
-          console.log("PUKLO GET Weather");
+    this.weatherService.getWeather().subscribe((response: any) => {
+        console.log( this.currentWeather);
+        this.currentWeather = new Weather(
+          response.name,
+          response.weather[0].description,
+          `https://openweathermap.org/img/w/${response.weather[0].icon}.png`,
+          response.main.temp,
+          response.main.temp_min,
+          response.main.temp_max,
+          response.main.humidity,
+          response.wind.speed
+        );
+        console.log( this.currentWeather);
       });
-    this.weatherService.getForecast()
-      .then((forecasts: Forecast[] | null) => {
-        if(forecasts)
-          this.forecasts = forecasts;
-        else
-          console.log("PUKLO GET Forecast");
-      });
+
+    this.weatherService.getForecast().subscribe((response: any) => {
+      console.log( this.forecasts);
+      const forecast = [];
+      for (let i = 0; i < response.list.length; i += 8) {
+        const item = response.list[i];
+        forecast.push(new Forecast(
+          new Date(item.dt_txt),
+          item.weather[0].description,
+          `https://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+          item.main.temp,
+          item.main.temp_min,
+          item.main.temp_max,
+          item.main.humidity,
+          item.wind.speed
+        ));
+      }
+      this.forecasts = forecast;
+      console.log( this.forecasts);
+    });
   }
 }
--- src/front/client/src/app/services/weather-forecast.service.ts
+++ src/front/client/src/app/services/weather-forecast.service.ts
@@ -12,53 +12,14 @@ export class WeatherForecastService {
 
   constructor(private http: HttpClient) {}
 
-  getWeather() :Promise<any>
+  getWeather() :Observable<any>
   {
-    return this.http.get(`${environment.apiUrl}weather/current`)
-      .toPromise()
-      .then((response: any) => {
-        return new Weather(
-          response.name,
-          response.weather[0].description,
-          `https://openweathermap.org/img/w/${response.weather[0].icon}.png`,
-          response.main.temp,
-          response.main.temp_min,
-          response.main.temp_max,
-          response.main.humidity,
-          response.wind.speed
-        );
-      })
-      .catch((error: any) => {
-        console.log(error);
-        return null;
-      });
+    return this.http.get(`${environment.apiUrl}weather/current`);
   }
 
-  getForecast() :Promise<any>
+  getForecast() :Observable<any>
   {
-    return this.http.get(`${environment.apiUrl}weather/forecast`)
-      .toPromise()
-      .then((response: any) => {
-        const forecast = [];
-        for (let i = 0; i < response.list.length; i += 8) {
-          const item = response.list[i];
-          forecast.push(new Forecast(
-            new Date(item.dt_txt),
-            item.weather[0].description,
-            `https://openweathermap.org/img/w/${item.weather[0].icon}.png`,
-            item.main.temp,
-            item.main.temp_min,
-            item.main.temp_max,
-            item.main.humidity,
-            item.wind.speed
-          ));
-        }
-        return forecast;
-      })
-      .catch((error: any) => {
-        console.log(error);
-        return null;
-      });
+    return this.http.get(`${environment.apiUrl}weather/forecast`);
   }
 }
 
