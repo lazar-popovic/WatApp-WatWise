f6ae4d5f4a306d42255bb60f05e50afd27e9b09c
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-04-21 13:40:15 +0200

    #133 Added wattage coluumn to DeviceType model. Added new migration for generating new DeviceTypes

--- src/back/API/Migrations/20230421111138_AddedWattageColumnInDeviceTypes.cs
+++ src/back/API/Migrations/20230421111138_AddedWattageColumnInDeviceTypes.cs
@@ -5,18 +5,24 @@
 namespace API.Migrations
 {
     /// <inheritdoc />
-    public partial class InsertDefaultDeviceTypes : Migration
+    public partial class AddedWattageColumnInDeviceTypes : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            
+            migrationBuilder.AddColumn<double>(
+                name: "WattageInkW",
+                table: "DeviceTypes",
+                type: "REAL",
+                nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "WattageInkW",
+                table: "DeviceTypes");
         }
     }
 }
