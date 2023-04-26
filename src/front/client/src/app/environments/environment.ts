7684cec50d4f5dbd395e1583139cefa95a6e552f
Bogdan Andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-04-26 23:29:28 +0200

    publish-branch: Forgot to add /api in enviroments

--- src/front/client/src/app/environments/environment.ts
+++ src/front/client/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: "http://softeng.pmf.kg.ac.rs:10081/",
+    apiUrl: "http://softeng.pmf.kg.ac.rs:10081/api/",
     pictureAppendix: "data:image/png;base64,"
 };
