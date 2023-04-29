94bbd595ec0582e393eb9626f6eb32b418cfec62
Lazar Popovic <popovic.lazar98@yahoo.com>
2023-04-29 21:40:21 +0200

    #150 Defined and added DeviceJob model to database with all necessary fields.

---
+++ src/back/API/Models/Entity/DeviceJob.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models.Entity;
+
+public class DeviceJob
+{
+    public int? Id { get; set; }
+    public int? DeviceId { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public int? StartJobId { get; set; }
+    public int? EndJobId { get; set; }
+    public bool? Turn { get; set; }
+    public bool? Repeat { get; set; }
+}
