6a8cc4b44b537b3845e4015ed67d00c77567c448
Lazar Popovic <popovic.lazar98@yahoo.com>
2023-04-30 16:08:15 +0200

    #150 Created ShedulerController with ScheduleDeviceActivity action method which receives DeviceJobViewModel as request and calls DeviceSceduling service which is in charge of sceduling jobs via Hangfire.

---
+++ src/back/API/API/SchedulerController.cs
@@ -0,0 +1,22 @@
+﻿using API.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.API;
+
+[ApiController]
+[Route("api/scheduler")]
+public class SchedulerController : ControllerBase
+{
+    private readonly IDeviceScheduling _deviceScheduling;
+
+    public SchedulerController(IDeviceScheduling deviceScheduling)
+    {
+        _deviceScheduling = deviceScheduling;
+    }
+
+    [HttpPost("device-job")]
+    public async Task<IActionResult> ScheduleDeviceActivity(DeviceJobViewModel request)
+    {
+        return Ok(await _deviceScheduling.ScheduleDeviceJob(request));
+    }
+}
