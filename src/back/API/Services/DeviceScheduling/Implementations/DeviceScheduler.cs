de008bc5ee1fe76a9e1ec14f0334faa07985c8cf
Lazar Popovic <popovic.lazar98@yahoo.com>
2023-04-30 16:35:28 +0200

    #150 implemented ExecuteJob method that gets device for given id,does checks about null reference and sets device ActivityStatus based on JobViewModel.

--- src/back/API/Services/DeviceScheduling/Implementations/DeviceScheduler.cs
+++ src/back/API/Services/DeviceScheduling/Implementations/DeviceScheduler.cs
@@ -1,14 +1,35 @@
 ﻿using API.Common;
+using API.Models;
+using API.Models.Entity;
 using API.Models.ViewModels;
 using API.Services.DeviceScheduling.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Services.DeviceScheduling.Implementations;
 
 public class DeviceScheduler : IDeviceScheduler
 {
-    public Task ExecuteJob(int deviceId, bool turn)
+    private readonly DataContext _dbContext;
+
+    public DeviceScheduler(DataContext dbContext)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
+    }
+
+    public async Task ExecuteJob(int deviceId, bool turn)
+    {
+        var device = await _dbContext.Devices.FirstOrDefaultAsync(device => device.Id == deviceId);
+
+        if (device != null)
+        {
+            device.ActivityStatus = turn;
+            await _dbContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new NullReferenceException("Given device does not exist!");
+        }
+       
     }
 
     public Task<Response?> ScheduleJob(DeviceJobViewModel request)
