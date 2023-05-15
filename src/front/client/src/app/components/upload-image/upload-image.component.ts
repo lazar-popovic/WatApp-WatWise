bf96f48660aee06d56028aa57d66cc0b1685449c
bogdan.andjelkovic <bogdan.andjelkovic02@gmail.com>
2023-05-15 03:57:46 +0200

    dev: Final push before testing

--- src/front/client/src/app/components/upload-image/upload-image.component.ts
+++ src/front/client/src/app/components/upload-image/upload-image.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
 import { AuthService } from 'src/app/services/auth-service.service';
+import { ToastrNotifService } from 'src/app/services/toastr-notif.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class UploadImageComponent {
   imageChangedEvent: any = '';
   croppedImage: any = '/assets/profile.jpeg';
 
-  constructor(private userService: UserService, private authService: AuthService) { }
+  constructor(private userService: UserService, private authService: AuthService, private toastrNotifService: ToastrNotifService) { }
 
     fileChangeEvent(event: any): void {
         this.imageChangedEvent = event;
@@ -36,17 +37,28 @@ export class UploadImageComponent {
     }
 
     uploadImage() {
-      this.data.id = this.authService.userId;
-      this.data.imagePicture = this.croppedImage.split("base64,")[1];
-      this.userService.uploadImage(this.data).subscribe((result :any) => {
-        this.hideUpload();
-      }, (error: any) => {
+      if( this.croppedImage != '/assets/profile.jpeg') {
+        this.data.id = this.authService.userId;
+        this.data.imagePicture = this.croppedImage.split("base64,")[1];
+        this.userService.uploadImage(this.data).subscribe((result :any) => {
+          if( result.body.success) {
+            this.toastrNotifService.showSuccess( result.body.data.message);
+            window.location.reload();
+          }
+          else {
+            this.toastrNotifService.showErrors( result.body.errors);
+          }
+        }, (error: any) => {
 
-      })
+        })
+      }
+      else {
+        this.toastrNotifService.showErrors(["Image not selected!"]);
+      }
     }
 
     hideUpload() {
       (document.querySelector(".profile-overview-overlay") as HTMLDivElement).style.display = "none";
     }
-  
+
 }
